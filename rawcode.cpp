2cf5a029500a89af8090659f8c9564cce8facd39
Jaime Murillo Espitia <jmurilloe@unal.edu.co>
2025-05-21 20:29:35 -0500

    Added general pseudo-code of just one LxL array.

---
+++ rawcode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main(void){
+
+    /*  Crear arreglo LxL Matriz probabilidades
+        Rellenar arreglo con numeros entre 0 y 1 
+        Indicando si está lleno
+        Indices
+        Crear arreglo LxL Matriz id
+        
+        Verificar si el arreglo tiene al menos un cluster percolante
+
+        Contar el numero de elementos de todos los clusters percolantes
+        sacar estadisticas (mean size and standard deviation)
+    */
+    
+}
